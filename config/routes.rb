80ef651b778071242ead310a4cdf1daea8ec7d0b
Ricielle Cheloni <riciellecheloni@gmail.com>
2021-07-11 22:23:59 -0300

    Configurando rota para página principal

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  
+  root to: 'site/welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
