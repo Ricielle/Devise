c870cdc18531b5913dca5404962d9bed2714772a
Ricielle Cheloni <riciellecheloni@gmail.com>
2021-07-11 22:32:12 -0300

    Instalando o Devise e corrigindo a rota

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   
-  root to: 'site/welcome#index"
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
